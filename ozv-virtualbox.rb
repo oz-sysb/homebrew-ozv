42ad72b5c638909046557192192d5f25dc93df57
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2020-06-29 23:07:20 +0900

    [MOD] Formura を追加

---
+++ ozv-virtualbox.rb
@@ -0,0 +1,66 @@
+cask 'ozv-virtualbox' do
+  version '5.2.32,132073'
+  sha256 '65e4983bd84b21eae4aef616e8a9edb4d7cd4dd862d0da0b4607f42a7db7e06c'
+
+  url "https://download.virtualbox.org/virtualbox/#{version.before_comma}/VirtualBox-#{version.before_comma}-#{version.after_comma}-OSX.dmg"
+  appcast 'https://download.virtualbox.org/virtualbox/LATEST.TXT'
+  appcast 'https://raw.githubusercontent.com/oz-sysb/homebrew-ozv/master/versions/versions/ozv-virtualbox.txt'
+  name 'Oracle VirtualBox'
+  homepage 'https://www.virtualbox.org/'
+
+  conflicts_with cask: 'virtualbox-beta'
+
+  pkg 'VirtualBox.pkg',
+      choices: [
+                 {
+                   'choiceIdentifier' => 'choiceVBoxKEXTs',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'choiceVBox',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'choiceVBoxCLI',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'choiceOSXFuseCore',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 0,
+                 },
+               ]
+
+  postflight do
+    # If VirtualBox is installed before `/usr/local/lib/pkgconfig` is created by Homebrew, it creates it itself with incorrect permissions that break other packages
+    # See https://github.com/Homebrew/homebrew-cask/issues/68730#issuecomment-534363026
+    set_ownership '/usr/local/lib/pkgconfig'
+  end
+
+  uninstall script:  {
+                       executable: 'VirtualBox_Uninstall.tool',
+                       args:       ['--unattended'],
+                       sudo:       true,
+                     },
+            pkgutil: 'org.virtualbox.pkg.*',
+            delete:  '/usr/local/bin/vboximg-mount'
+
+  zap trash: [
+               '/Library/Application Support/VirtualBox',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox.sfl*',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualboxvm.sfl*',
+               '~/Library/Preferences/org.virtualbox.app.VirtualBox.plist',
+               '~/Library/Preferences/org.virtualbox.app.VirtualBoxVM.plist',
+               '~/Library/Saved Application State/org.virtualbox.app.VirtualBox.savedState',
+               '~/Library/Saved Application State/org.virtualbox.app.VirtualBoxVM.savedState',
+               '~/Library/VirtualBox',
+             ],
+      rmdir: '~/VirtualBox VMs'
+
+  caveats do
+    kext
+  end
+end
